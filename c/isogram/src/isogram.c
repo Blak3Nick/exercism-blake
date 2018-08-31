fc33c2d2d99f8f30db1f367a71d3043f5e88b282
Blak3Nick <blaken2021@gmail.com>
2018-08-31 13:59:31 -0400

    testing

--- c/isogram/src/isogram.c
+++ c/isogram/src/isogram.c
@@ -3,25 +3,20 @@
 #include <stdio.h>
 
 bool is_isogram(const char phrase[]){
-  char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+  char alphabet[] = "abcdefghijklmnopqrstuvwxyz- ";
   int next =0;
   char c = phrase[next];
   c = tolower(c);
-
   char b;
   int found;
   while (c != '\0'){
-    if(c == ' ' || c == '-'){
-        //printf("True in check\n");
-        next ++;
-        c = phrase[next];
-        found = 1;
-    }
     found = 0;
-    for (int i=0; i< 27; i++){
+    for (int i=0; i< 28; i++){
       b = alphabet[i];
       if (b == c){
-        alphabet[i] = 1;
+        if(c != ' ' && c != '-'){
+            alphabet[i] = 1;
+        }
         next ++;
         c = phrase[next];
         found = 1;
