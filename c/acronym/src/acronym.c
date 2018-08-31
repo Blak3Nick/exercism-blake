fa136cff703d873389b581676e0caf0cde3067a0
Blak3Nick <blaken2021@gmail.com>
2018-08-31 18:25:01 -0400

    testing

--- c/acronym/src/acronym.c
+++ c/acronym/src/acronym.c
@@ -2,5 +2,11 @@
 #include <string.h>
 
 char *abbreviate(const char *phrase){
-    
+    char * acro;
+    size_t len = len(phrase);
+    char letters[len];
+    for (size_t i =0; i<len; i++){
+      
+    }
+    return acro;
 }
