c7643eb572de45efbaf583d2ff9f8eb9bf6d9e83
Blak3Nick <blaken2021@gmail.com>
2018-09-03 06:41:26 -0400

    changes

--- c/word-count/src/word_count.c
+++ c/word-count/src/word_count.c
@@ -4,8 +4,17 @@
 
 int word_count(const char *input_text, word_count_word_t * words){
   int len = strlen(input_text);
-  
+  char b;
+  size_t count1 =0;
+  size_t count2 = 0;
+  char mywords [20][50];
   for(int i=0; i<len; i++){
+    b = input_text[i];
+    if (b == ' '){
+      count1 ++;
+      count2 =0;
+      continue;
+    }
 
   }
 }
