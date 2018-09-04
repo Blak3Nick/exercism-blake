d433891148a3eed53bdc9d252e58ea7ccb160940
Blak3Nick <blaken2021@gmail.com>
2018-09-04 14:34:51 -0400

    testing

--- c/word-count/src/word_count.c
+++ c/word-count/src/word_count.c
@@ -7,11 +7,13 @@ int word_count(const char *input_text, word_count_word_t * words){
   printf("%s\n", input_text);
   printf("Words text are:\t%s\n", (*words).text);
   printf("Count is:\t%d\n", (*words).count);
+  char theword[len];
   char b;
   int count1 =0;
   //char theword[50];
   for(int i=0; i<len; i++){
     b = input_text[i];
+    theword[i] = b;
     if (b == ' '){
       (*words).count = count1+1;
 
@@ -21,8 +23,11 @@ int word_count(const char *input_text, word_count_word_t * words){
     (*words).text[i] = b;
     }
     if(len>0 && count1 <1 ){
-      count1 +=1;
-      (*words).text = input_text;
+      (*words).count = 1;
+      for (int j=0; j<len; j++){
+      (*words).text[j] = theword[j];
+      }
+
     }
     printf("The word was:\t");
     for(int i=0; i<5; i++){
