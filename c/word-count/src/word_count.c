5a737d726b324cc0fafbd2bfca30fde5f834540d
Blak3Nick <blaken2021@gmail.com>
2018-09-06 14:47:37 -0400

    Not understanding the response required to pass the tests

--- c/word-count/src/word_count.c
+++ c/word-count/src/word_count.c
@@ -1,28 +1,39 @@
 #include <stdio.h>
 #include <string.h>
+#include <stddef.h>
+#include <stdlib.h>
 #include "word_count.h"
 
 int word_count(const char *input_text, word_count_word_t * words){
   //words is a pointer to an array of word_count_word_t
   int len = strlen(input_text);
   printf("%s\n", input_text);
-  printf("Words text are:\t%s\n", (*words).text);
-  (*words).count =0;
-  printf("Count is:\t%d\n", (*words).count);
   char b;
+  int index = 0;
+  int indexForWords = 0;
+  int wordCount = 1;
+  char word[MAX_WORD_LENGTH];
+  words[indexForWords].count =1;
   //char theword[50];
   for(int i=0; i<len; i++){
     b = input_text[i];
     if (b == ' '){
-      (*words).count += 1;
-
-      (*words).text[i] = '\0';
+      word[index +1] = '\0';
+      char * theword = word;
+      int size = sizeof(word);
+      strncpy(words[indexForWords].text, theword, size);
+      words[indexForWords].count += 1;
+      wordCount ++;
+      for(int j=0; j<len; j++){
+        word[j] = '\0';
       }
-    (*words).text[i] = b;
     }
-    (*words).count ++;
+
+
+      word[index] = b;
+      index ++;
+    }
 
     printf("The word was:\t");
-    printf("%s\n", (*words).text);
-    return (*words).count;
+    return wordCount;
   }
