56b694e1993f3a7c823fea920157ab3212e99cc9
Blak3Nick <blaken2021@gmail.com>
2018-09-08 06:12:28 -0400

    test

--- c/word-count/test/mytest.c
+++ c/word-count/test/mytest.c
@@ -3,14 +3,14 @@
 #include <string.h>
 #include "vendor/unity.h"
 #include "../src/word_count.h"
-
+word_count_word_t  tests[1];
 void tester(word_count_word_t *wordArray){
 
-  printf("%d\n", (*wordArray).count);
+  printf("%d\n", wordArray[0].count);
 }
 
 int main(void) {
-  word_count_word_t * tests;
+
   tests[0].count = 1;
   strncpy(tests[0].text, "word", 5);
   tester(tests);
